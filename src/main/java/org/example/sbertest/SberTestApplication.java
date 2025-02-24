54d5049a60911b633021fe060f20015e54b0b0f2
Mihail <m.volkov@stv.dunice.net>
2025-02-24 13:38:35 +0300

    Initial commit

---
+++ src/main/java/org/example/sbertest/SberTestApplication.java
@@ -0,0 +1,13 @@
+package org.example.sbertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SberTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SberTestApplication.class, args);
+    }
+
+}
